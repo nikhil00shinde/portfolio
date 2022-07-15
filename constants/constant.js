ebca51f7fe8b99f41ef6f9183036b6c22d37f5cc
Nikhil Shinde <nikhilshinde00@outlook.com>
2022-07-15 19:38:10 +0530

    skills section

--- constants/constant.js
+++ constants/constant.js
@@ -1,6 +1,80 @@
+
 export const HomeSectionName = [
   {name:"Project",pos:{top:"0.8rem",left:"1rem"},class:"section_tl",link:"/project"},
   {name:"Experience",pos:{top:"0.8rem",right:"1rem"},class:"section_tr",link:"/experience"},
   {name:"Skills",pos: {bottom:"0.8rem",left:"1rem"},class:"section_br",link:"/skill"},
   {name:"Contact Me",pos:{bottom:"0.8rem",right:"1rem"},class:"section_bl",link:"/contactme"}
+]
+import ReactImg from "../images/react.gif";
+import JavaImg from "../images/java.svg";
+import PythonImg from "../images/python.svg";
+import javascript from "../images/javascript.svg";
+import html from "../images/html.svg";
+import css from "../images/css3.svg";
+import mui from "../images/material-ui.svg";
+import redux from "../images/redux.svg";
+import express from "../images/express.svg";
+import nodejs from "../images/nodejs.svg";
+import mongo from "../images/mongodb.png";
+import firebase from "../images/firebase.png";
+import git from "../images/git.svg";
+import pupp from "../images/puppeteer.png"
+
+export const skillName = [
+  {
+    name:"Java",
+    src:JavaImg
+  },
+  {
+    name:"Python",
+    src:PythonImg
+  },
+  {
+    name:"Javascript",
+    src:javascript
+  },
+  {
+    name:"HTML",
+    src:html
+  },
+  {
+    name:"CSS",
+    src:css
+  },
+  {
+    name:"Material UI",
+    src:mui
+  },
+  {
+    name:'React',
+    src:ReactImg
+  },
+  {
+    name:"Redux",
+    src:redux
+  },
+  {
+    name:"Express.js",
+    src:express
+  },
+  {
+    name:"Nodejs",
+    src:nodejs
+  },
+  {
+    name:"MongoDB",
+    src:mongo
+  },
+  {
+    name:"Firebase",
+    src:firebase
+  },
+  {
+    name:"Git/Github",
+    src:git
+  },
+  {
+    name:"Puppeteer",
+    src:pupp
+  }
 ]
--- pages/skill/index.js
+++ pages/skill/index.js
@@ -1,54 +1,62 @@
 import React from 'react';
 import classes from "../../styles/skills.module.css";
 import { Text } from '@chakra-ui/react';
-import { Box,SimpleGrid } from "@chakra-ui/react"
-
+import { Box,SimpleGrid,Center } from "@chakra-ui/react";
+import {motion } from "framer-motion";
+import Image from 'next/image';
+import { skillName } from '../../constants/constant';
 function Index() {
   return (
     <>
     <div className={classes.container}>
      <div className={classes.header}></div>
      <div className={classes.skillsC}>
-      <Text fontSize='3xl'>(3xl) In love with React & Next</Text>
+      <Text textAlign="center" fontSize='3xl'><bold>Skills</bold></Text>
       <SimpleGrid
+      textAlign="center"
   bg='gray.50'
-  columns={{ sm: 2, md: 4 }}
-  spacing='8'
+  columns={{ sm: 2, md: 3,lg:4 }}
+  spacing="8"
   p='10'
-  textAlign='center'
+  justifyContent= "center"
   rounded='lg'
   color='gray.400'
 >
-  <Box boxShadow='xs' p='6' rounded='md' bg='white'>
-    xs
-  </Box>
-  <Box boxShadow='sm' p='6' rounded='md' bg='white'>
-    sm
-  </Box>
-  <Box boxShadow='base' p='6' rounded='md' bg='white'>
-    Base
-  </Box>
-  <Box boxShadow='md' p='6' rounded='md' bg='white'>
-    md
-  </Box>
-  <Box boxShadow='lg' p='6' rounded='md' bg='white'>
-    lg
-  </Box>
-  <Box boxShadow='xl' p='6' rounded='md' bg='white'>
-    xl
-  </Box>
-  <Box boxShadow='2xl' p='6' rounded='md' bg='white'>
-    2xl
-  </Box>
-  <Box boxShadow='dark-lg' p='6' rounded='md' bg='white'>
-    Dark lg
-  </Box>
-  <Box boxShadow='outline' p='6' rounded='md' bg='white'>
-    Outline
-  </Box>
-  <Box boxShadow='inner' p='6' rounded='md' bg='white'>
-    Inner
-  </Box>
+  {
+      skillName.map((ele,id)=>(
+        <Box 
+        ml="2"
+        key={id}
+        as={motion.div}
+        height='50px'
+        width='200px'
+        boxShadow='md' 
+        p='2' 
+        rounded='md' 
+        bg='white' 
+        color="black"
+        transition='0.3s linear'
+        alignItems="center"
+        fontSize="1.2rem"
+        _hover={{
+          background: "white",
+          color: "rgba(49,49,55,1)",
+          boxShadow:'2xl'
+        }}>
+              <Center>
+          <Image 
+          src={ele.src}
+          alt="react"
+          width="30px"
+          height="30px" 
+          >
+          </Image>
+          <Text ml="3">{" "}{ele.name}</Text>
+          
+            </Center>
+        </Box>
+      ))
+  }
 </SimpleGrid>
       </div>
     </div>
